fb0bf6901f8fd8ced2025ca19ebd059adc1e0732
norjamillekasan <88083841+norjamillekasan@users.noreply.github.com>
2022-02-28 10:21:42 +0800

    re

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(InitialAccountSeeder::class);
+        $this->call(InitialAccountSeeder::class);
         $this->call(CourseSeeder::class);
     }
 }
